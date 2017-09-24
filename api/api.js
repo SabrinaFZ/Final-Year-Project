d686f115023975023677a648df180124a78e3b70
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-09-24 15:16:55 +0200

    db

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const pgp = require("pg-promise")()
 pgp.pg.defaults.ssl = true;
-pgp("postgres://lpombgdnyryjaq:1c788f5e971cb6fd08aa8599ce5ea09afe5061a8a1425fbcd55ea6f66811bca2@ec2-107-20-255-96.compute-1.amazonaws.com:5432/da10rpt0n0onqh")
+pgp("postgres://dxshgaaudfuttt:636bee3eefe840a62d9e2ebdeb3da441f5a580a900e65e4e279332911c5c8c3b@ec2-184-72-230-93.compute-1.amazonaws.com:5432/d5qmmgsv415fv1")
 
 //const db = pgp("")
 
