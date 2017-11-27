b5d9a9f342eb3defdff688db63a0dc51f6065a39
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-11-27 20:06:19 +0100

    Fix

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -108,7 +108,7 @@ router.post('/payment', (req, res, next) => {
     res.status(200)
       .json({
         status: 'success',
-        message: payload,
+        message: payload
       });
   }
 
