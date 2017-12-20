a84c690affb6d6247af2ba358ae5ea0df268b452
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-12-20 14:39:10 +0100

    Change settings

--- src/js/app/components/Settings/index.js
+++ src/js/app/components/Settings/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import common from './../../../../styles/'
 
 export default class Settings extends Component {
   constructor(props){
@@ -8,8 +11,10 @@ export default class Settings extends Component {
 
   render() {
     return (
-      <View>
-          <Text>Settings</Text>
+      <View style={[common.marginTop50, common.center]}>
+        <Icon name='build' type='MaterialIcons' size={100} color='#e9418b' />
+        <Text style={[common.marginTop20, common.textNormal, common.textCenter]}>Not implemented yet </Text>
+        <Text style={[common.textNormal, common.textCenter]}>Go back to App </Text>
       </View>
     );
   }
